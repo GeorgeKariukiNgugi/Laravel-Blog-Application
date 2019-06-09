4ba72d5d0580164689c95e7ecfdfb34e8fbb631d
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2019-06-09 13:51:36 +0300

    Edited the User Model.

--- app/User.php
+++ app/User.php
@@ -4,17 +4,28 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Password\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
 
-class User extends Authenticatable
+class User extends Model implements AuthenticatableContract,CanResetPasswordContract
 {
     use Notifiable;
+    use Authenticatable,CanResetPasswordContract;
+    use EntrustUserTrait;
+
+
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+
+     protected $table = 'users';
     protected $fillable = [
         'name', 'email', 'password',
     ];
